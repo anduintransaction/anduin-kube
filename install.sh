f711ba01765e91c114ef92139e1704bdebe49b62
keimoon <keimoonvie@gmail.com>
2016-12-26 12:44:08 +0700

    Fix install

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-here=`cd $(dirname $BASH_SOURCE); pwd`
-. $here/lib/functions/update.sh
+function update {
+    which git > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "git not found"
+        exit 1
+    fi
+    rm -rf anduin-kube && \
+        git clone https://github.com/anduintransaction/anduin-kube && \
+        ./anduin-kube/bin/install.sh
+    exitCode=$?
+    rm -rf anduin-kube
+    exit $exitCode
+}
 
 update
