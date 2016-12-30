7c49189847d62902ee867a7f29bddf7c3d0f2d9d
keimoon <keimoonvie@gmail.com>
2016-12-30 09:58:50 +0700

    Update fix command

--- lib/functions/fix.sh
+++ lib/functions/fix.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 function fix {
-    runCommandOnMinikube "sudo udhcpc"
+    runCommandOnMinikube "sudo ifconfig eth0 down"
+    runCommandOnMinikube "sudo ifconfig eth0 up"
+    pid=`runCommandOnMinikube "sudo cat /var/run/udhcpc.eth0.pid 2>/dev/null"`
+    if [ ! -z "$pid" ]; then
+        runCommandOnMinikube "sudo kill $pid 2>/dev/null"
+        
+    fi
+    runCommandOnMinikube "sudo /sbin/udhcpc -b -i eth0 -x hostname minikube -p /var/run/udhcpc.eth0.pid"
 }
