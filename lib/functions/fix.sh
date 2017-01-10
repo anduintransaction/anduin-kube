bb0edf16408661593f75996867aee10ce694ff62
keimoon <keimoonvie@gmail.com>
2017-01-10 13:15:21 +0700

    Fix command, upgrade imladris

--- lib/functions/fix.sh
+++ lib/functions/fix.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 function fix {
-    runCommandOnMinikube "sudo ifconfig eth0 down"
-    runCommandOnMinikube "sudo ifconfig eth0 up"
-    pid=`runCommandOnMinikube "sudo cat /var/run/udhcpc.eth0.pid 2>/dev/null"`
-    if [ ! -z "$pid" ]; then
-        runCommandOnMinikube "sudo kill $pid 2>/dev/null"
-        
-    fi
-    runCommandOnMinikube "sudo /sbin/udhcpc -b -i eth0 -x hostname minikube -p /var/run/udhcpc.eth0.pid"
+    VBoxManage natnetwork stop --netname minikube && \
+        VBoxManage natnetwork start --netname minikube && \
+        runCommandOnMinikube "sudo systemctl restart docker"
 }
