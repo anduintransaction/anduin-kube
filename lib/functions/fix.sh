4a869305c4fac6ebbf5907c2ca066318d6b2d33c
keimoon <keimoonvie@gmail.com>
2017-03-20 18:49:23 +0700

    Fix anduin-kube fix

--- lib/functions/fix.sh
+++ lib/functions/fix.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 function fix {
-    VBoxManage natnetwork stop --netname minikube && \
-        VBoxManage natnetwork start --netname minikube && \
-        runCommandOnMinikube "sudo systemctl restart docker" && \
-        cleanupDNS && \
+    anduin-kube force-stop
+    cleanupDNS && \
         cleanupRoute && \
-        modifyDNS && \
-        modifyRoute && \
-        runCommandOnMinikube "sudo /mnt/sda1/var/init.d/coredns.sh stop" && \
-        runCommandOnMinikube "sudo /mnt/sda1/var/init.d/coredns.sh start"
+        VBoxManage modifyvm minikube --nic1 none && \
+        VBoxManage modifyvm minikube --nic1 nat && \
+        anduin-kube start
 }
