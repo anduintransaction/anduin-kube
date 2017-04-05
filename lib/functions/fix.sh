f2e8ab52287e75b32808eed4c417abbb0577fa9e
keimoon <keimoonvie@gmail.com>
2017-04-05 13:08:52 +0700

    Update fix command to issue ipv6 problem

--- lib/functions/fix.sh
+++ lib/functions/fix.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 function fix {
+    runCommandOnMinikube sudo ifconfig eth0 down
+    sleep 1
+    VBoxManage controlvm minikube nic1 null
+    sleep 1
+    VBoxManage controlvm minikube nic1 nat
+    sleep 1
+    runCommandOnMinikube sudo ifconfig eth0 up
+    sleep 1
     sudo anduin-kube setup-network
 }
